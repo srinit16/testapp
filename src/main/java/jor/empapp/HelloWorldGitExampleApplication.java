67814b1706bc2e0c6c0a60021fcccb482f1e7978
srinit16 <srinit16@gmail.com>
2022-08-10 11:02:36 -0400

    updated -project

---
+++ src/main/java/jor/empapp/HelloWorldGitExampleApplication.java
@@ -0,0 +1,14 @@
+package jor.empapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldGitExampleApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/jor/empapp/HelloWorldGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jor.empapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
